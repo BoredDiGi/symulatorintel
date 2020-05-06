005dd98971243a38e89985e1540dfcd2e9d31d91
BoredDiGi <61658897+BoredDiGi@users.noreply.github.com>
2020-05-06 15:35:11 +0200

    yes

---
+++ script.js
@@ -0,0 +1,8 @@
+function sym(){
+    var ax = document.getElementById("ax");
+    var bx = document.getElementById("bx");
+    var cx = document.getElementById("cx");
+    var dx = document.getElementById("dx");
+    var div = document.getElementById("bx");
+    bx.value = ax.value;
+}
