393de010bdd67a4117f5aee0c79a55198741f635
BoredDiGi <61658897+BoredDiGi@users.noreply.github.com>
2020-07-18 10:21:44 +0200

    all changes

--- script.js
+++ script.js
@@ -1,8 +1,73 @@
-function sym(){
+function sym() {
     var ax = document.getElementById("ax");
     var bx = document.getElementById("bx");
     var cx = document.getElementById("cx");
     var dx = document.getElementById("dx");
-    var div = document.getElementById("bx");
-    bx.value = ax.value;
+    var par1 = document.getElementById("par1");
+    var par2 = document.getElementById("par2");
+    if (par1.value == "ax" && par2.value == "bx") {
+        ax.value = bx.value;
+    }
+    else
+        if (par1.value == "ax" && par2.value == "cx") {
+            ax.value = cx.value;
+        }
+        else
+            if (par1.value == "ax" && par2.value == "dx") {
+                ax.value = dx.value;
+            }
+            else
+                if (par1.value == "ax" && par2.value == "ax") {
+                    ax.value = ax.value;
+                }
+                else
+                    if (par1.value == "bx" && par2.value == "ax") {
+                        bx.value = ax.value;
+                    }
+                    else
+                        if (par1.value == "bx" && par2.value == "cx") {
+                            bx.value = cx.value;
+                        }
+                        else
+                            if (par1.value == "bx" && par2.value == "dx") {
+                                bx.value = dx.value;
+                            }
+                            else
+                                if (par1.value == "bx" && par2.value == "bx") {
+                                    bx.value = bx.value;
+                                }
+                                else
+                                    if (par1.value == "cx" && par2.value == "ax") {
+                                        cx.value = ax.value;
+                                    }
+                                    else
+                                        if (par1.value == "cx" && par2.value == "bx") {
+                                            cx.value = bx.value;
+                                        }
+                                        else
+                                            if (par1.value == "cx" && par2.value == "dx") {
+                                                cx.value = dx.value;
+                                            }
+                                            else
+                                                if (par1.value == "cx" && par2.value == "cx") {
+                                                    cx.value = cx.value;
+                                                }
+                                                else
+                                                    if (par1.value == "dx" && par2.value == "ax") {
+                                                        dx.value = ax.value;
+                                                    }
+                                                    else
+                                                        if (par1.value == "dx" && par2.value == "bx") {
+                                                            dx.value = bx.value;
+                                                        }
+                                                        else
+                                                            if (par1.value == "dx" && par2.value == "cx") {
+                                                                dx.value = cx.value;
+                                                            }
+                                                            else
+                                                                if (par1.value == "dx" && par2.value == "dx") {
+                                                                    dx.value = dx.value;
+                                                                }
+                                                                else
+                                                                    alert("cos poszlo nie tak :/");
 }
